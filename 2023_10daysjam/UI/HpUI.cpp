c464ace26b150b2f59b88c5a26588afd04225f3e
美鈴 武田 <k022g1076@m.neec.ac.jp>
2023-10-23 15:36:56 +0900

    一回HP画像写してみる

--- 2023_10daysjam/UI/HpUI.cpp
+++ 2023_10daysjam/UI/HpUI.cpp
@@ -1,1 +1,31 @@
 ﻿#include "HpUI.h"
+
+void HpUI::Initialize()
+{
+	//左上端
+	Lpos_ = { 200,100 };
+	//右下端
+	Rpos_ = { sizeX_,sizeY_ };
+	//画像
+	image_ = Novice::LoadTexture("./Resource/images/HPBar_kari.png");
+	
+	//移動した合計
+	moveX_ = 0;
+	//移動する量(HPが1たび増減するたびに動く量)
+	moveSpeedX_ = int(0.88f);
+}
+
+void HpUI::Update(float hp)
+{
+
+}
+
+
+void HpUI::Draw()
+{
+	//決まった範囲に画像写すタイプ
+	//Novice::DrawSpriteRect();
+	//画像写す範囲を徐々に狭めていくタイプ
+	Novice::DrawQuad(Lpos_.x_, Lpos_.y_, Rpos_.x_, Lpos_.y_, Lpos_.x_, Rpos_.y_, Rpos_.x_, Rpos_.y_
+	,0,0, sizeX_, sizeY_, image_,WHITE);
+}
--- 2023_10daysjam/UI/HpUI.h
+++ 2023_10daysjam/UI/HpUI.h
@@ -1,7 +1,15 @@
 ﻿#pragma once
 #include <Vector2.h>
+#include <ScreenSize.h>
 #include <Novice.h>
 #include "ImGuiManager.h"
+
+//Hpゲージ用のっぽい何か(まとめたかった)
+//Noviceの画像処理がint統一なのでintでまとめる
+typedef struct HpState {
+	int x_;
+	int y_;
+};
 
 /// <summary>
 /// HP表示のクラス
@@ -10,17 +18,39 @@ class HpUI
 {
 public:
 
+	//~HpUI();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 毎フレーム処理
+	/// </summary>
+	void Update(float hp);
+
+	/// <summary>
+	/// 前景描画
+	/// </summary>
+	void Draw();
 
 private:
 
 	//左端
-	Vector2 Lpos_ = {0.0f,0.0f};
+	HpState Lpos_ = {0,0};
 	//右端
-	Vector2 Rpos_ = { 0.0f,0.0f };
+	HpState Rpos_ = { 0,0 };
 	//画像
 	int image_= 0;
-	//画像サイズ
-	const int sizeX = 440;
-	const int sizeY = 64;
+	//写したいサイズ
+	const int sizeX_ = 440;
+	const int sizeY_ = 64;
+
+	//移動した合計
+	int moveX_ = 0;
+	//移動する量(HPが1減るたびに動く量)
+	int moveSpeedX_ = int(0.88f);
+
 };
 
